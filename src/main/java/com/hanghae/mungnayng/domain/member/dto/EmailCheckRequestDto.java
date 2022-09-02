b2d3c49e5293cddb7d560450147e4fcb01265930
HoisubLee <5chips17@gmail.com>
2022-09-02 09:25:24 +0900

    <feat> @NoArgsConstructor 추가

--- src/main/java/com/hanghae/mungnayng/domain/member/dto/EmailCheckRequestDto.java
+++ src/main/java/com/hanghae/mungnayng/domain/member/dto/EmailCheckRequestDto.java
@@ -1,8 +1,10 @@
 package com.hanghae.mungnayng.domain.member.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class EmailCheckRequestDto {
     private String email;
 }
--- src/main/java/com/hanghae/mungnayng/domain/member/dto/LoginRequestDto.java
+++ src/main/java/com/hanghae/mungnayng/domain/member/dto/LoginRequestDto.java
@@ -2,8 +2,10 @@ package com.hanghae.mungnayng.domain.member.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequestDto {
     private String email;
