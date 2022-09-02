592684e249df9f9fcacaae1c92306f8dcdad0bb2
HoisubLee <5chips17@gmail.com>
2022-09-02 17:12:22 +0900

    <feat> 닉네임 중복 검사 api 구현

--- src/main/java/com/hanghae/mungnayng/domain/member/Member.java
+++ src/main/java/com/hanghae/mungnayng/domain/member/Member.java
@@ -19,7 +19,7 @@ public class Member extends Timestamped {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String nickname;
 
     @Column(nullable = false)
--- src/main/java/com/hanghae/mungnayng/repository/MemberRepository.java
+++ src/main/java/com/hanghae/mungnayng/repository/MemberRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
     boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
     Optional<Member> findByEmail(String email);
 }
