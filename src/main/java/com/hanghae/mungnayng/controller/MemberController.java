651827105a3607c78260033395febf7b632f80b0
HoisubLee <5chips17@gmail.com>
2022-09-06 17:17:45 +0900

    <bug> 로그인 시, response로 닉네임 전달, return값 ResposeEntity로 통일

--- src/main/java/com/hanghae/mungnayng/controller/MemberController.java
+++ src/main/java/com/hanghae/mungnayng/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.hanghae.mungnayng.service.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -23,41 +24,40 @@ public class MemberController {
     /* 회원 가입 */
     @ApiOperation(value = "회원가입 메소드")
     @PostMapping("/members/signup")
-    public SignupResponseDto signup(@RequestBody SignupRequestDto signupRequestDto) {
+    public ResponseEntity<?> signup(@RequestBody SignupRequestDto signupRequestDto) {
         memberService.signUp(signupRequestDto);
-        return new SignupResponseDto("회원가입 성공", true);
+        return ResponseEntity.ok(new SignupResponseDto("회원가입 성공", true));
     }
 
     /* 이메일 중복 검사 */
     @ApiOperation(value = "이메일 중복 검사 메소드")
     @PostMapping("/members/email-check")
-    public SignupResponseDto emailcheck(@RequestBody EmailCheckRequestDto emailCheckRequestDto) {
+    public ResponseEntity<?> emailcheck(@RequestBody EmailCheckRequestDto emailCheckRequestDto) {
         memberService.checkEmailIsDuplicate(emailCheckRequestDto.getEmail());
-        return new SignupResponseDto("가입이 가능한 회원입니다.", true);
+        return ResponseEntity.ok(new SignupResponseDto("가입이 가능한 회원입니다.", true));
     }
 
     /* 닉네임 중복 검사 */
     @ApiOperation(value = "닉네임 중복 검사 메소드")
     @PostMapping("/members/nickname-check")
-    public SignupResponseDto nicknamecheck(@RequestBody NicknameCheckRequestDto nicknameCheckRequestDto) {
+    public ResponseEntity<?> nicknamecheck(@RequestBody NicknameCheckRequestDto nicknameCheckRequestDto) {
         memberService.checkNicknameIsDuplicate(nicknameCheckRequestDto.getNickname());
-        return new SignupResponseDto("사용 가능한 닉네임입니다.", true);
+        return ResponseEntity.ok(new SignupResponseDto("사용 가능한 닉네임입니다.", true));
     }
 
     /* 로그인 */
     @ApiOperation(value = "로그인 메소드")
     @PostMapping("/members/login")
-    public SignupResponseDto login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
-        memberService.login(loginRequestDto, response);
-        return new SignupResponseDto("로그인 성공.", true);
+    public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        return ResponseEntity.ok(memberService.login(loginRequestDto, response));
     }
 
     /* 로그 아웃 */
     @ApiOperation(value = "로그 아웃 메소드")
     @PostMapping("/members/logout")
-    public SignupResponseDto logout(@AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<?> logout(@AuthenticationPrincipal UserDetails userDetails) {
         Member member = ((UserDetailsImpl) userDetails).getMember();
         memberService.logout(member);
-        return new SignupResponseDto("로그아웃 성공.", true);
+        return ResponseEntity.ok(new SignupResponseDto("로그아웃 성공.", true));
     }
 }
---
+++ src/main/java/com/hanghae/mungnayng/domain/member/dto/LoginResponseDto.java
@@ -0,0 +1,11 @@
+package com.hanghae.mungnayng.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class LoginResponseDto {
+    private String nickname;
+    private boolean success;
+}
