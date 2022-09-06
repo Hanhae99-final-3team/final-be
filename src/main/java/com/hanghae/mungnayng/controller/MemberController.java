c62f398ba43777b900ad53185ccf069fd19e43a2
HoisubLee <5chips17@gmail.com>
2022-09-06 16:58:56 +0900

    <docs> 회원가입, 로그인 api에 swagger설명 추가

--- src/main/java/com/hanghae/mungnayng/controller/MemberController.java
+++ src/main/java/com/hanghae/mungnayng/controller/MemberController.java
@@ -4,6 +4,8 @@ import com.hanghae.mungnayng.domain.UserDetailsImpl;
 import com.hanghae.mungnayng.domain.member.Member;
 import com.hanghae.mungnayng.domain.member.dto.*;
 import com.hanghae.mungnayng.service.MemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Api(tags = {"회원 API 정보를 제공하는 Controller"})
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
@@ -18,6 +21,7 @@ public class MemberController {
     private final MemberService memberService;
 
     /* 회원 가입 */
+    @ApiOperation(value = "회원가입 메소드")
     @PostMapping("/members/signup")
     public SignupResponseDto signup(@RequestBody SignupRequestDto signupRequestDto) {
         memberService.signUp(signupRequestDto);
@@ -25,6 +29,7 @@ public class MemberController {
     }
 
     /* 이메일 중복 검사 */
+    @ApiOperation(value = "이메일 중복 검사 메소드")
     @PostMapping("/members/email-check")
     public SignupResponseDto emailcheck(@RequestBody EmailCheckRequestDto emailCheckRequestDto) {
         memberService.checkEmailIsDuplicate(emailCheckRequestDto.getEmail());
@@ -32,6 +37,7 @@ public class MemberController {
     }
 
     /* 닉네임 중복 검사 */
+    @ApiOperation(value = "닉네임 중복 검사 메소드")
     @PostMapping("/members/nickname-check")
     public SignupResponseDto nicknamecheck(@RequestBody NicknameCheckRequestDto nicknameCheckRequestDto) {
         memberService.checkNicknameIsDuplicate(nicknameCheckRequestDto.getNickname());
@@ -39,6 +45,7 @@ public class MemberController {
     }
 
     /* 로그인 */
+    @ApiOperation(value = "로그인 메소드")
     @PostMapping("/members/login")
     public SignupResponseDto login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
         memberService.login(loginRequestDto, response);
@@ -46,6 +53,7 @@ public class MemberController {
     }
 
     /* 로그 아웃 */
+    @ApiOperation(value = "로그 아웃 메소드")
     @PostMapping("/members/logout")
     public SignupResponseDto logout(@AuthenticationPrincipal UserDetails userDetails) {
         Member member = ((UserDetailsImpl) userDetails).getMember();
