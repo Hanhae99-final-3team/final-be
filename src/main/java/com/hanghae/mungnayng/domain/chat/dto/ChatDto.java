a91ac7958b70bd7db430c34281ed07a3e6b69832
1argeD <94172354+1argeD@users.noreply.github.com>
2022-10-06 18:35:28 +0900

    <chore> 채팅 시간을 현재 시간에 맞춤

--- src/main/java/com/hanghae/mungnayng/domain/chat/dto/ChatDto.java
+++ src/main/java/com/hanghae/mungnayng/domain/chat/dto/ChatDto.java
@@ -7,8 +7,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 
 @Getter
@@ -26,12 +26,13 @@ public class ChatDto {
 
 
     public ChatDto (Chat chat) {
+        TimeZone tzSeoul = TimeZone.getTimeZone("Asia/Seoul");
         this.chaId=chat.getId();
         Member member = chat.getRoomDetail().getMember();
         this.memberId = member.getMemberId();
 //        this.proPic = member.getProPic();
         this.content = chat.getMessage();
         this.roomInfoId = chat.getRoomInfoId();
-        this.createdAt = chat.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").withZone(ZoneId.of("Asia/Seoul")));
+        this.createdAt = chat.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").withZone(tzSeoul.toZoneId()));
     }
 }
